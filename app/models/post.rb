210393eaa274d5e5814246a23299f6f6092f55d2
Todd Krieger <twkriege@mtu.edu>
2021-11-29 12:14:51 -0500

    commit tree b01ec18df1bcec7a779935798a911124dc2b53ca

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait
+
+    def clickbait
+    
+        if self.title
+            return errors.add(:title, "no one is going to read this...") unless (self.title.include? "Won't Believe") || (self.title.include? "Secret") || (self.title.include? "Guess") || (self.title.include? "Top")
+        end
+    end
 end
